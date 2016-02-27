b6e6d6f4850c2092efd43c2bdd62e5dc0982e90f
demijade <sy_judy@yahoo.com>
2016-02-27 23:03:59 +0800

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227141051) do
+ActiveRecord::Schema.define(version: 20160227142353) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,5 +34,15 @@ ActiveRecord::Schema.define(version: 20160227141051) do
 
   add_index "customers", ["email"], name: "index_customers_on_email", unique: true
   add_index "customers", ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+
+  create_table "requests", force: :cascade do |t|
+    t.string   "number_of_riders"
+    t.string   "drop_off"
+    t.string   "pick_up"
+    t.datetime "time_requested"
+    t.datetime "pick_up_time"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
 end
