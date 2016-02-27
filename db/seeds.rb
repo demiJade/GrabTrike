946f54e9001c7baa5dc720d21ad711893a3fc659
demijade <sy_judy@yahoo.com>
2016-02-28 00:09:43 +0800

    home controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = Customer.create(email: "admin@grabtrike.com", first_name: "Admin", last_name:"Admin", password: "12345678", password_confirmation: "12345678")
+admin.toggle!(:admin)
+admin.save!
